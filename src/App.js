9835de85dd5889ae287d8633a603192e4ec992e7
L_Ryu <ryuichi.1479xxx@gmail.com>
2024-06-29 18:17:18 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,134 @@
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `${winner} is Winner!!`;
+  } else {
+    status = `Next Player is ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+  }
+
+  return (
+    <>
+      <h1>三目並べ</h1>
+      <h3>ルール</h3>
+      <p>2名で行うゲーム。</p>
+      <p>オセロのようにXかOかを決めて交互に枠内をクリックする。</p>
+      <h3>勝利条件</h3>
+      <p>先に3つ同じマークを並べることが出来た方が勝利。 </p>
+      <h1>{status}</h1>
+      <div className="board-row">
+        <Square
+          value={squares[0]}
+          onSquareClick={() => {
+            handleClick(0);
+          }}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => {
+            handleClick(1);
+          }}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => {
+            handleClick(2);
+          }}
+        />
+      </div>
+      <div className="board-row">
+        <Square
+          value={squares[3]}
+          onSquareClick={() => {
+            handleClick(3);
+          }}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => {
+            handleClick(4);
+          }}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => {
+            handleClick(5);
+          }}
+        />
+      </div>
+      <div className="board-row">
+        <Square
+          value={squares[6]}
+          onSquareClick={() => {
+            handleClick(6);
+          }}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => {
+            handleClick(7);
+          }}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => {
+            handleClick(8);
+          }}
+        />
+      </div>
+      <button onClick={doReload}>最初から遊ぶ</button>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function doReload() {
+  window.location.reload();
+}
